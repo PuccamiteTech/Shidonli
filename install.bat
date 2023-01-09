ee3517302edab15610d5b65123ad69f2516b6efd
PuccamiteTech <53921171+PuccamiteTech@users.noreply.github.com>
2023-01-09 00:29:49 -0500

    Update WAMP stack

--- install.bat
+++ install.bat
@@ -4,7 +4,7 @@ copy /b hosts.bak+hosts.add %tmp%\hosts
 cls
 cd bin
 wget "https://ftp.mozilla.org/pub/firefox/releases/52.2.1esr/win64/en-US/Firefox Setup 52.2.1esr.exe"
-wget https://downloads.bitnami.com/files/stacks/wampstack/8.1.8-0/bitnami-wampstack-8.1.8-0-windows-x64-installer.exe
+wget https://downloads.bitnami.com/files/stacks/wampstack/8.1.12-0/bitnami-wampstack-8.1.12-0-windows-x64-installer.exe
 title Local Shidonni Server AutoTool
 Silverlight_x64.exe /q /doNotRequireDRMPrompt /noupdate
 "Firefox Setup 52.2.1esr.exe" -ms -ma
@@ -12,12 +12,12 @@ ates.lnk
 cls
 echo Uncheck all boxes as you manually install the following application. The password is your choice, but leave the stack's directory unchanged!
 pause
-bitnami-wampstack-8.1.8-0-windows-x64-installer.exe
-del /Q C:\Bitnami\wampstack-8.1.8-0\apache2\htdocs\*.*
-del /Q C:\Bitnami\wampstack-8.1.8-0\apache2\htdocs\img\*.*
-copy htdocs.zip C:\Bitnami\wampstack-8.1.8-0\apache2\
+bitnami-wampstack-8.1.12-0-windows-x64-installer.exe
+del /Q C:\Bitnami\wampstack-8.1.12-0\apache2\htdocs\*.*
+del /Q C:\Bitnami\wampstack-8.1.12-0\apache2\htdocs\img\*.*
+copy htdocs.zip C:\Bitnami\wampstack-8.1.12-0\apache2\
 cls
-cd C:\Bitnami\wampstack-8.1.8-0\apache2\
+cd C:\Bitnami\wampstack-8.1.12-0\apache2\
 rd htdocs\img
 powershell Expand-Archive htdocs.v2.zip
 del htdocs.zip
